1f655f789e5861c1dab795a90d5769d6fdd52c28
ivonin85 <ivonin85@gmail.com>
2019-10-07 00:15:28 +0300

    first commit

---
+++ src/test/java/CSVWriter.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class CSVWriter {
+
+    public static void main(String[] args) {
+
+        try (PrintWriter writer = new PrintWriter(new File("test1.csv"))) {
+
+            StringBuilder sb = new StringBuilder();
+            sb.append("id,");
+            sb.append("Name");
+            sb.append('\n');
+
+            sb.append("1,");
+            sb.append("Prashant Ghimire");
+            sb.append('\n');
+
+            writer.write(sb.toString());
+
+            System.out.println("done!");
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
